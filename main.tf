12eca2bd0de79d3aac2fcc559d4be0418b71998c
Luan Amorim <luanrodriguesamorim@gmail.com>
2025-12-26 13:08:36 -0300

    subindo terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c398cb65a93047f2"
+  instance_type = "t3.small"
+
+  key_name = "iac-alura"
+
+  tags = {
+    Name = "Instancia Terraform"
+  }
+}
